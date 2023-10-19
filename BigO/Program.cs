b164586aec4267e1e8891a0688dcabb5e2462eac
Nasibb <nasibgi@code.edu.az>
2023-10-19 22:02:34 +0400

    BigO

---
+++ BigO/Program.cs
@@ -0,0 +1,54 @@
+﻿#region 2. a ve b ededinin cemi 2-reqemlidirse "cem ikireqemli ededdir" cem 3 reqemlidirse "cem ucreqemlidir" eks halda "nezere alinmayıb" cap edilsin
+
+//int a =int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int sum = a + b;
+//int count = 0;
+//while (sum != 0)
+//{
+//    sum /= 10;
+//    count++;
+//}
+//if (count == 2)
+//{
+//    Console.WriteLine("2 reqemli");
+//}
+//else if (count==3)
+//{
+//    Console.WriteLine("3 reqemli");
+//}
+//else
+//{
+//    Console.WriteLine("nezere alinmiyib...");
+//}
+
+#endregion
+
+#region 3.Verilmiş düzbucaqlı şkafın hər hansı bir qapıdan keçib-keçməyəcəyini  tapan algoritm yazın.
+
+using System.ComponentModel.Design;
+
+int skafHeight = int.Parse(Console.ReadLine());
+int skafWidth = int.Parse(Console.ReadLine());
+int skafLength = int.Parse(Console.ReadLine());
+int qapiHeight = int.Parse(Console.ReadLine());
+int qapiWidth = int.Parse(Console.ReadLine());
+
+if (
+   (( skafHeight == qapiHeight) && (skafWidth = qapiWidth)) || ((skafHeight == qapiHeight) && (skafLength = qapiWidth))
+   || ((skafLength == qapiWidth) && (skafWidth == qapiHeight)) || ((skafHeight == qapiWidth) && ((skafWidth == qapiHeight))
+   || ((skafLength == qapiHeight) && (skafHeight == qapiWidth)) || ((skafLength == qapiHeight) && (skafWidth == qapiWidth))
+    ))
+{ 
+
+    Console.WriteLine("kecir");
+}
+else
+{
+    Console.WriteLine("kecmir...");
+}
+
+   
+
+
+#endregion
